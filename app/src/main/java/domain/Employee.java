882f6fee07c3bea4d6977cad514732e5f1e09a02
Khine <khinezar70122107@gmail.com>
2023-06-21 12:14:56 -1200

    project lombok

--- app/src/main/java/domain/Employee.java
+++ app/src/main/java/domain/Employee.java
@@ -1,7 +1,22 @@
 package domain;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
-    private Long id;
+    private Integer id;
     private String firstName;
     private String lastName;
+    private Address address;
 }
+
